0c4a31a3e73c98a288ea0c1062b02226a98c024c
Mack Stump <mack.stump@gmail.com>
2016-05-11 14:40:18 -0400

    send join messages to connected/connecting clients

--- ws.go
+++ ws.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"sort"
 	"sync"
+	"time"
 )
 
 var upgrader = websocket.Upgrader{
@@ -17,6 +20,26 @@ type message struct {
 	Data string `json:"data"`
 }
 
+type user struct {
+	name string
+}
+
+type Users []user
+
+func (u Users) Len() int {
+	return len(u)
+}
+
+func (u Users) Less(i, j int) bool {
+	return u[i].name < u[j].name
+}
+
+func (u Users) Swap(i, j int) {
+	u[i], u[j] = u[j], u[i]
+}
+
+var users = make(Users, 0)
+
 type connection struct {
 	ws *websocket.Conn
 }
@@ -27,6 +50,13 @@ var connections = struct {
 }{
 	sync.Mutex{},
 	make(map[*connection]struct{}),
+}
+
+func genUsername() string {
+	now := time.Now()
+	digits := fmt.Sprintf("%v", now.UnixNano())
+
+	return digits[11:]
 }
 
 func cleanupConnection(c *connection) {
@@ -64,7 +94,34 @@ func Websocket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// generate a new username until we find one that doesn't exist
+	// in our users slice
+	name := genUsername()
+	for sort.Search(len(users), func(i int) bool { return users[i].name == name }) != len(users) {
+		name = genUsername()
+	}
+
+	// add generated user to users slice and sort by name
+	users = append(users, user{name: name})
+	sort.Sort(users)
+
 	c := &connection{ws: ws}
 	connections.conns[c] = struct{}{}
 	go readLoop(c)
+
+	// tell all connected clients that user has joined
+	msg := message{Type: "join", Data: name}
+	for conn, _ := range connections.conns {
+		conn.ws.WriteJSON(msg)
+	}
+
+	// tell this client about all other users
+	for _, user := range users {
+		if user.name == name {
+			continue
+		}
+
+		msg = message{Type: "join", Data: user.name}
+		c.ws.WriteJSON(msg)
+	}
 }
