7563d95d152f80912b0342b6d9f6f4c53946b171
Mack Stump <mack.stump@gmail.com>
2016-05-13 11:58:51 -0400

    add <MessageList> element to app

--- static/scripts/app.tsx
+++ static/scripts/app.tsx
@@ -1,11 +1,15 @@
 /// <reference path="typings/main.d.ts" />
 import UserList from './userlist';
+import MessageList from './messagelist';
 import ChatSocket from './chatsocket';
 
 let ws = new ChatSocket('ws://' + window.location.host + '/ws');
 
 ReactDOM.render(
-  <UserList chatsocket={ws} />,
+  <div>
+    <UserList chatsocket={ws} />
+    <MessageList chatsocket={ws} />
+  </div>,
   document.getElementById('content')
 );
 
